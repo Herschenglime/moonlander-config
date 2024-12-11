444d03bf40ab77df1f4ef846dacbd0556f66f39a
Herschenglime <herschenglime@gmail.com>
2024-12-10 21:21:01 -0500

    some local changes

--- MWQRK/config.h
+++ MWQRK/config.h
@@ -11,6 +11,8 @@
 #define PERMISSIVE_HOLD
 
 #define COMBO_TERM 25
+// let combos fire from every layer
+#define COMBO_ONLY_FROM_LAYER 0
 
 #undef RGB_MATRIX_TIMEOUT
 #define RGB_MATRIX_TIMEOUT 300000
