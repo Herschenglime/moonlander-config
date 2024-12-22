caa2bb9112d06f5b6a6aee861766a3502cf316c9
Herschenglime <herschenglime@gmail.com>
2024-12-22 18:40:24 -0500

    some local changes

--- merge_configs.sh
+++ merge_configs.sh
@@ -3,13 +3,26 @@ set -e # exit on error instead of continuing
 
 # turn this into a nix thing later if I get a ton of time - would fix the annoyances of shell script and skip the need for docker
 
+# set layout id and geometry - modify here to be default
+layout_id="NWALw"
+layout_geometry="moonlander"
+
+layout_path="$(pwd)/${layout_id}"
+
+# Set keyboard directory and make prefix
+keyboard_directory="$HOME/dev/qmk_firmware/keyboards/zsa"
+make_prefix="zsa/"
+
+
+# get any code changes from qmk_firmware
+
+# sync qmk_firmware keymap to moonlander-config
+rsync -rauv "${keyboard_directory}/${layout_geometry}/keymaps/${layout_id}/" "$(readlink -f $layout_path)"
+
 # commit unstaged changes right away to streamline dev process
 git commit -a -m "some local changes" || echo "No new local changes"
 git push
 
-# set layout id and geometry - modify here to be default
-layout_id="NWALw"
-layout_geometry="moonlander"
 
 # checkout oryx branch of repo to pull changes into
 git checkout oryx
@@ -36,7 +49,6 @@ echo change_description=${change_description}
 unzip -oj source.zip '*_source/*' -d ${layout_id}
 rm source.zip
 
-layout_path="$(pwd)/${layout_id}"
 
 # Commit and Push changes
 git add $layout_id
@@ -57,22 +69,15 @@ git push
 # git submodule update --init --recursive
 
 # Build the layout
-# Set keyboard directory and make prefix based on firmware version
-if [ "${firmware_version}" -ge 24 ]; then
-  keyboard_directory="$HOME/dev/qmk_firmware/keyboards/zsa"
-  make_prefix="zsa/"
-else
-  keyboard_directory="$HOME/dev/qmk_firmware/keyboards"
-  make_prefix=""
-fi
-
 # just skip symlinking so it doesn't recompile each time
 # forcibly symlink the folder so my changes actually make it through the copy
 # ln -sf "$(readlink -f $layout_path)" "${keyboard_directory}/${layout_geometry}/keymaps/"
 
 # use rsync to copy folder directly so edits can be carried out in qmk_firmware
 # with proper lsp support
-#
+# since code edits are made from within qmk_firmware, copy changes to this repo, merge, then send back
+
+
 # sync moonlander-config keymap to qmk_firmware
 rsync -rauv "$(readlink -f $layout_path)" "${keyboard_directory}/${layout_geometry}/keymaps/"
 
